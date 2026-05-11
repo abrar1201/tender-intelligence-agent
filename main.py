33052bc81a55bd4ef7e974a273b9de406c7a0ae2
abrar1201 <146915872+abrar1201@users.noreply.github.com>
2026-05-11 17:12:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def pick_top_with_source_balance(tenders, total=15, per_source_min=3):
 SCRAPERS = {
     "uk":            scrape_uk,
     # "findatender":   scrape_findatender,
-    "ted":           scrape_ted,
+    # "ted":           scrape_ted,
     "samgov":        scrape_samgov,
     # "worldbank":     scrape_worldbank,
     # "adb":           scrape_adb,
